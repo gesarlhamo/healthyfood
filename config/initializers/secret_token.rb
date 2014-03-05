c30ad0f9e31e3d76cfff3d2315019d5a59db58cb
gesarlhamo <gesarlhamo80@gmail.com>
2014-03-05 17:27:13 -0600

    basic frame of the site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Healthyfood::Application.config.secret_token = '0a7bf289f109ddbcd946bd29d47dae16a582f4db1deca32f69f99c631cf5dcf036aa5f5a0468cfc40bdf4877e485f928edafc2f42479f479e6ef2515d5de8ae5'
